d0c0c4e2d9af43e6d3cbd81ad85122519c092c02
Alpha DIOP <alphakhoss@gmail.com>
2023-12-20 17:25:36 +0000

    fix: cors

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://mern-authentication-delta.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   }),
